d44b1466cc775d96011e8eb03e59f14c7a152359
alecruste <algirdas.rusteika@reactseals.com>
2019-07-03 19:03:40 +0300

    icons

--- projectConfig/firebase.js
+++ projectConfig/firebase.js
@@ -3,24 +3,14 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-  apiKey: 'AIzaSyDYbaUW5g8pG3_xaqKlnAdX-Vz2Gkm7lKs',
-  authDomain: 'rnv-chat-template.firebaseapp.com',
-  databaseURL: 'https://rnv-chat-template.firebaseio.com',
-  projectId: 'rnv-chat-template',
-  storageBucket: 'rnv-chat-template.appspot.com',
-  messagingSenderId: '203089953823',
-  appId: '1:203089953823:web:34f3bcd96ae7bc37',
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  projectId: '<your-cloud-firestore-project>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-sender-id>',
+  appId: '<your-app-id>',
 };
-
-// const config = {
-//   apiKey: '<your-api-key>',
-//   authDomain: '<your-auth-domain>',
-//   databaseURL: '<your-database-url>',
-//   projectId: '<your-cloud-firestore-project>',
-//   storageBucket: '<your-storage-bucket>',
-//   messagingSenderId: '<your-sender-id>',
-//   appId: '<your-app-id>',
-// };
 
 firebase.initializeApp(config);
 
