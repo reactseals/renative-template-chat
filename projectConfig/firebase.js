67f016bb5c2129421afa2f7d3d74d1edfd9acda3
alecruste <algirdas.rusteika@reactseals.com>
2019-09-06 17:38:20 +0300

    rnv version update to 0.26.9

--- projectConfig/firebase.js
+++ projectConfig/firebase.js
@@ -1,16 +1,16 @@
-import firebase from '@firebase/app'; // UN-COMMENT THIS ONCE CONFIGURED
-// import firebase from './firebaseFake'; // REMOVE THIS ONCE CONFIGURED
+// import firebase from '@firebase/app'; // UN-COMMENT THIS ONCE CONFIGURED
+import firebase from './firebaseFake'; // REMOVE THIS ONCE CONFIGURED
 import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-    apiKey: 'AIzaSyDYbaUW5g8pG3_xaqKlnAdX-Vz2Gkm7lKs',
-    authDomain: 'rnv-chat-template.firebaseapp.com',
-    databaseURL: 'https://rnv-chat-template.firebaseio.com',
-    projectId: 'rnv-chat-template',
-    storageBucket: 'rnv-chat-template.appspot.com',
-    messagingSenderId: '203089953823',
-    appId: '1:203089953823:web:34f3bcd96ae7bc37',
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    projectId: '<your-cloud-firestore-project>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-sender-id>',
+    appId: '<your-app-id>',
 };
 
 firebase.initializeApp(config);
