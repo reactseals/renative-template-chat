7d3a5f7a4a8db7ceb5b3a1917c41d483e8277b73
DomantasMk <domantas.mikelionis@gmail.com>
2020-11-24 11:23:28 +0200

    React Native and Rnv version upgrade

--- projectConfig/firebase.js
+++ projectConfig/firebase.js
@@ -1,9 +1,9 @@
-// import firebase from '@firebase/app'; // UN-COMMENT THIS ONCE CONFIGURED
-import firebase from './firebaseFake'; // REMOVE THIS ONCE CONFIGURED
+import firebase from '@firebase/app'; // UN-COMMENT THIS ONCE CONFIGURED
+// import firebase from './firebaseFake'; // REMOVE THIS ONCE CONFIGURED
 import 'firebase/database';
 import 'firebase/storage';
 
-const config = {
+/* const config = {
     apiKey: '<your-api-key>',
     authDomain: '<your-auth-domain>',
     databaseURL: '<your-database-url>',
@@ -12,7 +12,16 @@ const config = {
     messagingSenderId: '<your-sender-id>',
     appId: '<your-app-id>',
 };
-
+ */
+const config = {
+    apiKey: "AIzaSyBe_QxdEBWOr7dJ-xLZuK1ceZCUULKNu0Q",
+    authDomain: "rnv-template-chat.firebaseapp.com",
+    databaseURL: "https://rnv-template-chat.firebaseio.com",
+    projectId: "rnv-template-chat",
+    storageBucket: "rnv-template-chat.appspot.com",
+    messagingSenderId: "654798159212",
+    appId: "1:654798159212:web:04e932e2e4cd42cc158749"
+  };
 firebase.initializeApp(config);
 
 export default firebase;
