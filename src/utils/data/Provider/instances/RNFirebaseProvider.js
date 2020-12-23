2d9e522d86464f4cd73978339af13e6f8f4aabaf
DomantasMk <domantas.mikelionis@gmail.com>
2020-12-23 10:09:42 +0200

    prettier/eslint for windows edited, removes unnecessary hocs

--- src/utils/data/Provider/instances/RNFirebaseProvider.js
+++ src/utils/data/Provider/instances/RNFirebaseProvider.js
@@ -13,22 +13,24 @@ class FirebaseProvider {
         chatRoom.on('value', (snap) => {
             if (snap.val()) {
                 console.log('whups');
-                EventRegister.emit('message', this.toArray(snap.val()));
+                EventRegister.emit('message', this.toArray(snap));
             }
         });
     };
 
     toArray = (firebaseRes) => {
         // turns firebase response JSON to an array
-        return Object.keys(firebaseRes).map((id) => {
-            return { uuid: id, ...firebaseRes[id] };
+        let keys = firebaseRes._snapshot.childKeys;
+        let data = firebaseRes.val();
+        return keys.map((id) => {
+            return { uuid: id, ...data[id] };
         });
     };
 
     getMessages = async () => {
         const messages = await chatRoom.once('value');
         if (messages.exists()) {
-            return this.toArray(messages.val());
+            return this.toArray(messages);
         }
 
         return [];
--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -1,5 +1,5 @@
 import DataProvider from './Provider/Provider';
-import DataProviderInstance from './Provider/instances/FirebaseProvider';
+import DataProviderInstance from './Provider/instances/RNFirebaseProvider';
 import Comment from './Model/Comment';
 
 const ProviderInstance = new DataProvider(new DataProviderInstance());
